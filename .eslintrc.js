9c983b6ab403e583f9bdc1f5b30fa192c75ad644
Hoan Trinh <hoantv@vuihoc.vn>
2022-11-01 10:12:38 +0700

    refactor: add commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
+  ignorePatterns: [".eslintrc.js", "commitlint.config.js"],
   rules: {
     "no-var": "error",
     semi: "error",
